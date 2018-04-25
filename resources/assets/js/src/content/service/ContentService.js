0e442211c56addc84884122e4fc6fb1bc8148f33
Serge <popov.ser92@gmail.com>
2018-04-25 17:04:48 +0300

    add auth

---
+++ resources/assets/js/src/content/service/ContentService.js
@@ -0,0 +1,48 @@
+import {RequestService} from "./../../common/service/RequestService";
+import {AuthService} from './../../auth/service/AuthService';
+import {Redirect} from 'react-router-dom';
+
+export class ContentService {
+
+    getContentType(name) {
+        switch (name) {
+            case 'articles':
+                return '/api/articles';
+                break;
+            default:
+                return false;
+        }
+    };
+
+    constructor() {
+        this.headers = {'Authorization': 'Bearer ' + localStorage.getItem('api_token')};
+        this.requestService = new RequestService();
+        this.authService = new AuthService();
+    }
+
+    getContent(contentType, args) {
+
+        let path = this.getContentType(contentType);
+
+        let params = {
+            method: args.method,
+            headers: this.headers
+        };
+
+        if (args.params) {
+            params.body = JSON.stringify(args.params);
+        }
+
+        if (!this.authService.checkToken()) {
+            return <Redirect to = "login"/>
+        }
+
+        return this.requestService.fetch(path, params).then(res => {
+                if (res.data) {
+                    console.log('Returned: ', res.data.meta.totalCount);
+                }
+                return Promise.resolve(res.data);
+            }
+        );
+    }
+}
