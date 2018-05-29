19599f3ff63111e659c71c58efe813020e7c04b6
Serge <popov.ser92@gmail.com>
2018-05-29 13:42:57 +0300

    add link to linkedin profile

--- resources/assets/js/src/auth/Profile.js
+++ resources/assets/js/src/auth/Profile.js
@@ -23,10 +23,11 @@ export class Profile extends Component {
                     <img src="https://media.licdn.com/dms/image/C5603AQHUfn8R04INKg/profile-displayphoto-shrink_200_200/0?e=1533168000&v=beta&t=0KhzRvIupZQPpNOqRi8P9nr_5LFQDh9X78D_A_wJfoI" className="rounded-circle" alt="User Image"/>
                 </div>
                 <div className="user-panel__info">
-                    <p className="user-panel__info-title">Developed by Popov Sergey</p>
+                    <p className="user-panel__info-title">
+                        Developed by <a href="https://www.linkedin.com/in/popovserge/" target="_blank"><i className="fab fa-linkedin-in"></i>Popov Sergey</a>
+                    </p>
 
                     <p>Recourse : <a href="http://gog.com" target="_blank">gog.com</a></p>
-                    {/*<button type="button" className="user-panel__info-status user-panel__info-status--online" onClick={this.handleLogout.bind(this)}>Logout</button>*/}
                 </div>
             </div>
         );
